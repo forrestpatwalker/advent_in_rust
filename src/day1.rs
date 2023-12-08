1c9d4733d2de413268738ae60af595c8b7146711
Forrest Walker <forrestpatwalker@hotmail.com>
2023-12-08 11:16:45 -0700

    solution for day1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -31,40 +31,75 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 
+Your puzzle answer was 55172.
+
+The first half of this puzzle is complete! It provides one gold star: *
+--- Part Two ---
+
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+
+
 --- Full problem: https://adventofcode.com/2023/day/1 ---
 */
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::path::Path;
-use std::collections::HashMap;
+use std::collections::{HashMap, BTreeMap};
 
 pub fn run() {
     let path = Path::new("input_files/day1_input.txt");
     let file = File::open(&path).unwrap();
     let reader = BufReader::new(file);
-
     let mut total_value = 0;
 
     for line in reader.lines() {
+        let numbers:HashMap<&str, char> = HashMap::from([
+            ("zero", '0'),
+            ("one", '1'),
+            ("two", '2'),
+            ("three", '3'),
+            ("four", '4'),
+            ("five", '5'),
+            ("six", '6'),
+            ("seven", '7'),
+            ("eight", '8'),
+            ("nine", '9'),
+            ]);
 
-        // let mut digit_hash: HashMap<usize, i32> = HashMap::new();
-        // let calibration_string = line.unwrap();
-        // let mut char_indices: Vec<(usize, char)> = calibration_string.char_indices().collect();
+        let mut digit_hash: BTreeMap<usize, char> = BTreeMap::new();
+        let calibration_string = line.unwrap();
+        let char_indices: Vec<(usize, char)> = calibration_string.char_indices().collect();
 
-        // for (index, ch) in char_indices {
-        //     if ch.is_digit(10) {
-        //         digit_hash
-        //     }
-        // }
+        for (index, ch) in char_indices {
+            if ch.is_digit(10) {
+                digit_hash.insert(index, ch);
+            }
+        }
+        
+        for (key, val) in numbers {
+            let mut start = 0;
+            while let Some(found) = calibration_string[start..].find(key) {
+                digit_hash.insert(found + start, val);
+                start += found + 1;
+            }
+        }
 
-
-
-        // let first_num = calibration_string.chars_indices().collect().iter().find(|&(index, c)| c.is_digit(10));
-        // let last_num = calibration_string.chars().rev().find(|&(index, c)| c.is_digit(10));
-
-        // let combined = format!("{}{}", first_num.unwrap().1, last_num.unwrap());
-
-        // total_value += combined.parse::<i32>().unwrap();
+        let combined = format!("{}{}", digit_hash.iter().next().unwrap().1, digit_hash.iter().next_back().unwrap().1);
+        total_value += combined.parse::<i32>().unwrap();
     }
 
     println!("The sum of all of the calibration values is: {}", total_value);
